7bd45f7cce750507367118c6f80c536bfa5a9e79
kx0651 <kx0651@kit.edu>
2023-08-25 15:14:16 +0200

    Initial_push

---
+++ 1_rgh_class/rgh_class.py
@@ -0,0 +1,369 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  5 12:13:02 2023
+
+@author: Jiasheng
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as sps
+import pandas as pd
+from tkinter import filedialog
+from tensorflow.keras.models import load_model
+plt.rcParams['text.usetex'] = True
+from scipy.interpolate import RegularGridInterpolator
+class rgh():
+    def __init__(self,x,z,y):
+        if (y.shape[0]!=len(z)) | (y.shape[1]!=len(x)):
+            print("Spanwise pixels of the roughness patch:" + str(len(z)))
+            print("Streamwise pixels of the roughness patch:" + str(len(x)))
+            print("Resolution of the roughness patch:" + str(y.shape))
+            raise ValueError("Incompatible roughness map size to the given coordinates")
+        if y.std()<0.001:
+            print("The detected wall fluctuation is too small, please check height scaling before use.")
+        x=x.reshape((-1))
+        z=z.reshape((-1))
+        ##### Interpolating surface to desired resolution
+        xnew=np.linspace(0,np.max(x),int(np.max(x)*500))
+        znew=np.linspace(0,np.max(z),int(np.max(z)*500))
+        [xnewM,znewM]=np.meshgrid(xnew,znew)
+        f=RegularGridInterpolator((x,z),y.T)
+        ynew=f((xnewM,znewM))
+        ##### Calculating desired quantities
+        del x,z,y
+        self.y=ynew
+        self.x=xnew
+        self.z=znew
+        self.Lx=np.max(xnew)
+        self.Lz=np.max(znew)
+        self.Nx=len(xnew)
+        self.Nz=len(znew)
+        self.dx=self.Lx/self.Nx
+        self.dz=self.Lz/self.Nz
+        self.kt=np.max(ynew)
+        self.sk=sps.skew(ynew,axis=None)
+        self.ku=sps.kurtosis(ynew,fisher=False,axis=None)
+        self.krms=np.std(ynew)
+        self.kmd=np.mean(ynew)
+        self.ra=np.mean(np.abs(ynew-np.mean(ynew)))
+        self.por=1-np.mean(ynew)/np.max(ynew)
+        self.ESx=np.mean(np.abs((np.roll(ynew,1,axis=0)-ynew)/self.dx))
+        self.ESz=np.mean(np.abs((np.roll(ynew,1,axis=1)-ynew)/self.dz))
+        self.incx=np.arctan(sps.skew((np.roll(ynew,1,axis=0)-ynew)/self.dx,axis=None)/2)
+        self.incz=np.arctan(sps.skew((np.roll(ynew,1,axis=1)-ynew)/self.dz,axis=None)/2)
+        self.PS=np.fft.fft2(ynew-np.mean(ynew))
+
+    def show_surface(self,representation="2D"): ## Plotting surface
+        if representation =="2D":
+            plt.imshow(self.y,extent=[self.x.min(),self.x.max(),self.z.min(),self.z.max()])
+            plt.colorbar()
+        elif (representation !="3D") & ((representation !="3d")):
+            print("Only 3D or 2D are the potions, setting to default 2D")
+            plt.imshow(self.y,extent=[self.x.min(),self.x.max(),self.z.min(),self.z.max()])
+            plt.colorbar()
+        else:
+            plt.figure()
+            ax = plt.axes(projection='3d')
+            XM,ZM=np.meshgrid(self.x,self.z)
+            ax.plot_surface(XM,ZM,self.y,linewidth=0,antialiased=False,cmap="viridis")
+            ax.set_xlim3d([self.x.min(),self.x.max()])
+            ax.set_ylim3d([self.z.min(),self.z.max()])
+            ax.set_zlim3d([0,4*(self.y.max()-self.y.min())])
+            ax.set_box_aspect([self.x.max(), self.z.max(), 1.0])
+
+    def print_stat(self): ## Outputing roughness statistical parameters as a table
+        return pd.DataFrame({"Length":[self.Lx],"Width":[self.Lz],"Sk":[self.sk],"Ku":self.ku,"k_RMS":self.krms,"k_md":self.kmd,
+                "kt":[self.kt],"Ra":[self.ra],"Por":[self.por],"ES_x":[self.ESx],
+                "ES_z":[self.ESz],"Inc_x":[self.incx],"Inc_z":[self.incz]})
+    
+
+    def plot_PDF(self,n_bins=10,Normalization=True): ## Plotting roughness PDF with desired number of bins
+        plt.hist(np.reshape(self.y,(-1)),bins=n_bins,density=Normalization)
+
+
+    def plot_PS(self,Normalization=True,azimuthal_average=False,moving_average=False,n_iter=3): ## Plotting roughness PS with desired setting
+        if (azimuthal_average==False) & (moving_average==True): ## Always do azimuthal average before moving average
+            print("moving average only applicable for azimuthal_average=True, thus azimuthal_average is automatically set to True")
+            azimuthal_average=True
+
+        # create "wave number" vectors, the unit is grid size
+        qx = np.linspace(-self.Nx/2, self.Nx/2-1, self.Nx)
+        qz = np.linspace(-self.Nz/2, self.Nz/2-1, self.Nz)
+        qx = qx * 2 * np.pi/self.Nx
+        qz = qz * 2 * np.pi/self.Nz
+        qxM, qzM = np.meshgrid(qx, qz)
+        ## Calculate the map of wavenumbers with same resolution as the surface, the origin is centered
+        q_radius = np.sqrt(qxM**2 + qzM**2)
+
+        # compute absolute value of power spectral density (imaginary part is not zero)
+        if Normalization==True:
+            self.PS[0,0]=0
+            PS_toplot = abs(self.PS)**2/(np.sum(abs(self.PS)**2)/(self.Nx*self.Nz))
+        else:
+            PS_toplot =abs(self.PS)**2
+        ## Do fftshift so that the PS map and wavenumber map are correspondent
+        PS_toplot=np.fft.fftshift(PS_toplot)
+
+        if azimuthal_average:
+            q_radius=np.reshape(q_radius,(-1))
+            PS_toplot=np.reshape(PS_toplot,(-1))
+            #sorting PS and q based on q
+            ind_q=np.argsort(q_radius)
+            PS_sorted=PS_toplot[ind_q]
+            q_sorted=q_radius[ind_q]
+            q_before=q_sorted[0]
+            q_averaged=np.zeros(len(np.unique(q_sorted)))
+            PS_averaged=np.zeros(len(np.unique(q_sorted)))
+            ind=0
+            ind_q=0
+            counter=0
+            for i in q_sorted:
+                if i==q_before:
+                    #sum PS with same q
+                    PS_averaged[ind]=PS_averaged[ind]+PS_sorted[ind_q]
+                    counter=counter+1
+                else:
+                    #divided by the number of same q
+                    PS_averaged[ind]=PS_averaged[ind]/counter
+                    q_averaged[ind]=q_before
+                    counter=1
+                    ind=ind+1
+                    q_before=i
+                    #start to sum PS with next same q
+                    PS_averaged[ind]=PS_averaged[ind]+PS_sorted[ind_q]
+                if ind_q==len(q_sorted):# ending
+                    PS_averaged[ind]=PS_averaged[ind]/counter
+                    q_averaged[ind]=q_before
+                ind_q=ind_q+1
+            if moving_average:
+                q_radius_1d=q_averaged
+                PS_1d=PS_averaged
+                PS_1d_averaged=np.zeros(len(q_radius_1d))
+                for iter in range(n_iter):
+                    for i in range(len(q_radius_1d)):
+                        if i==0:
+                            PS_1d_averaged[i]=PS_1d[0]
+                        elif i==1:
+                            PS_1d_averaged[i]=np.mean([PS_1d[0],PS_1d[1]])
+                        elif i==len(q_radius_1d)-2:
+                            PS_1d_averaged[i]=np.mean([PS_1d[i],PS_1d[i+1]])
+                        elif i==len(q_radius_1d)-1:
+                            PS_1d_averaged[i]=np.mean([PS_1d[i]])
+                        else:
+                            PS_1d_averaged[i]=np.mean([PS_1d[i+2],PS_1d[i+1],PS_1d[i-2],PS_1d[i-1],PS_1d[i]])
+                    PS_1d=PS_1d_averaged
+                PS_1d_pooled=[]
+                Q_1d_bak=q_radius_1d
+                pool=0
+                Q_1d_pooled=[]
+                for i in range(len(q_radius_1d)):
+                    if i==0:
+                        Q_1d_pooled.append(Q_1d_bak[i])
+                        PS_1d_pooled.append(max([PS_1d[0]]))
+                        continue
+                    elif pool==3:
+                        Q_1d_pooled.append(Q_1d_bak[i-1])
+                        PS_1d_pooled.append(max([PS_1d[i],PS_1d[i-1],PS_1d[i-2]]))
+                        pool=0
+                        continue
+                    elif i==len(q_radius_1d)-1:
+                        Q_1d_pooled.append(Q_1d_bak[i-1])
+                        PS_1d_pooled.append(max([PS_1d[i],PS_1d[i-1],PS_1d[i-2]]))
+                    pool=pool+1
+                q_radius_1d=np.array(Q_1d_pooled)
+                PS_toplot_1d=np.array(PS_1d_pooled)
+            else:
+                q_radius_1d=q_averaged
+                PS_toplot_1d=PS_averaged
+        else:
+            ind = np.argsort(q_radius.reshape(-1))    
+            q_radius_1d = q_radius.reshape(-1)[ind]        
+            PS_toplot_1d = PS_toplot.reshape(-1)[ind]
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(q_radius_1d, PS_toplot_1d,c='k',marker="*")
+        #ax.scatter(q_sorted, PS_sorted,c='k',marker="*")
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        ax.set_ylabel("PS")
+        ax.set_xlabel(r"$2\pi \Delta/\lambda$")
+        
+    def FFT_filter(self,lmax,lmin):
+        qx = np.linspace(-self.Nx/2, self.Nx/2-1, self.Nx)
+        qz = np.linspace(-self.Nz/2, self.Nz/2-1, self.Nz)
+        qx = qx * 2 * np.pi/self.Nx
+        qz = qz * 2 * np.pi/self.Nz
+        qxM, qzM = np.meshgrid(qx, qz)
+        q_radius = np.sqrt(qxM**2 + qzM**2)
+        lmax = lmax * 500
+        lmin = lmin * 500
+        q0 = 2 * np.pi/lmax 
+        q1 = 2 * np.pi/lmin
+        PS_new = np.fft.fftshift(self.PS)
+        # filter out PS contents out side of the desired range
+        PS_new[q_radius>q1] = 0
+        PS_new[q_radius<q0] = 0
+        PS_new=np.fft.fftshift(PS_new)
+        # return a new rgh object
+        return rgh(self.x,self.z,np.real(np.fft.ifft2(PS_new))-np.min(np.real(np.fft.ifft2(PS_new))))
+        
+
+
+    def get_model_input(self,lmax=2,lmin=0.04,azimuthal_average=False,moving_average=False,n_iter=3):
+        surface=self.y-np.min(self.y)
+        
+        n99,bin99=np.histogram(surface.reshape((-1)),density=True,bins=100)
+        bin99=(bin99[1:]+bin99[:-1])/2
+        C_I=0.01
+        flag=0
+        for i in range(len(n99)):
+            s=np.sum(n99[:i])/np.sum(n99)
+            if (s>C_I/2) & (flag==0):
+                LBound=bin99[i]
+                flag=1
+            if (s>1-C_I/2) & (flag==1):
+                UBound=bin99[i]
+                break
+        
+        
+        k99=UBound-LBound
+        # lambda values for output, normalized by k99
+        lambda_0=lmax/k99
+        lambda_1=lmin/k99
+
+
+        # convert to same unit 
+        lmax = lmax * 500
+        lmin = lmin * 500
+        q0 = 2 * np.pi/lmax 
+        q1 = 2 * np.pi/lmin
+        
+        # This part similar to plot_PS()
+        if (azimuthal_average==False) & moving_average:
+            print("moving average only applicable for azimuthal_average=True, thus azimuthal_average is automatically set to True")
+            azimuthal_average=True
+        # create "wave number" vectors
+        qx = np.linspace(-self.Nx/2, self.Nx/2-1, self.Nx)
+        qz = np.linspace(-self.Nz/2, self.Nz/2-1, self.Nz)
+        qx = qx * 2 * np.pi/self.Nx
+        qz = qz * 2 * np.pi/self.Nz
+        qxM, qzM = np.meshgrid(qx, qz)
+        q_radius = np.sqrt(qxM**2 + qzM**2)
+          
+        PS_toplot = abs(self.PS)/np.sqrt((np.sum(abs(self.PS)**2)/(self.Nx*self.Nz)))
+        PS_toplot=np.fft.fftshift(PS_toplot)
+        if azimuthal_average:
+            q_radius=np.reshape(q_radius,(-1))
+            PS_toplot=np.reshape(PS_toplot,(-1))
+            ind_q=np.argsort(q_radius)
+            PS_sorted=PS_toplot[ind_q]
+            q_sorted=q_radius[ind_q]
+            q_before=q_sorted[0]
+            q_averaged=np.zeros(len(np.unique(q_sorted)))
+            PS_averaged=np.zeros(len(np.unique(q_sorted)))
+            ind=0
+            ind_q=0
+            counter=0
+            for i in q_sorted:
+                if i==q_before:
+                    PS_averaged[ind]=PS_averaged[ind]+PS_sorted[ind_q]
+                    counter=counter+1
+                else:
+                    PS_averaged[ind]=PS_averaged[ind]/counter
+                    q_averaged[ind]=q_before
+                    counter=1
+                    ind=ind+1
+                    q_before=i
+                    PS_averaged[ind]=PS_averaged[ind]+PS_sorted[ind_q]
+                if ind_q==len(q_sorted):
+                    PS_averaged[ind]=PS_averaged[ind]/counter
+                    q_averaged[ind]=q_before
+                ind_q=ind_q+1
+            if moving_average:
+                q_radius_1d=q_averaged
+                PS_1d=PS_averaged
+                PS_1d_averaged=np.zeros(len(q_radius_1d))
+                for iter in range(n_iter):
+                    for i in range(len(q_radius_1d)):
+                        if i==0:
+                            PS_1d_averaged[i]=PS_1d[0]
+                        elif i==1:
+                            PS_1d_averaged[i]=np.mean([PS_1d[0],PS_1d[1]])
+                        elif i==len(q_radius_1d)-2:
+                            PS_1d_averaged[i]=np.mean([PS_1d[i],PS_1d[i+1]])
+                        elif i==len(q_radius_1d)-1:
+                            PS_1d_averaged[i]=np.mean([PS_1d[i]])
+                        else:
+                            PS_1d_averaged[i]=np.mean([PS_1d[i+2],PS_1d[i+1],PS_1d[i-2],PS_1d[i-1],PS_1d[i]])
+                    PS_1d=PS_1d_averaged
+                PS_1d_pooled=[]
+                Q_1d_bak=q_radius_1d
+                pool=0
+                Q_1d_pooled=[]
+                for i in range(len(q_radius_1d)):
+                    if i==0:
+                        Q_1d_pooled.append(Q_1d_bak[i])
+                        PS_1d_pooled.append(max([PS_1d[0]]))
+                        continue
+                    elif pool==3:
+                        Q_1d_pooled.append(Q_1d_bak[i-1])
+                        PS_1d_pooled.append(max([PS_1d[i-1],PS_1d[i],PS_1d[i-2]]))
+                        pool=0
+                        continue
+                    elif i==len(q_radius_1d)-1:
+                        Q_1d_pooled.append(Q_1d_bak[i-1])
+                        PS_1d_pooled.append(max([PS_1d[i-1],PS_1d[i],PS_1d[i-2]]))
+                    pool=pool+1
+                q_radius_1d=np.array(Q_1d_pooled)
+                PS_toplot_1d=np.array(PS_1d_pooled)
+            else:
+                q_radius_1d=q_averaged
+                PS_toplot_1d=PS_averaged
+        else:
+            ind = np.argsort(q_radius.reshape(-1))    
+            q_radius_1d = q_radius.reshape(-1)[ind]        
+            PS_toplot_1d = PS_toplot.reshape(-1)[ind]
+        ## Selecting 30 points from processed PS curve
+        ## equidistant in log scaling
+        Qquerys=10**np.linspace(np.log10(q0),np.log10(q1),30)
+        PSquerys=np.zeros(len(Qquerys))
+        PSquerys[0]=PS_toplot_1d[q_radius_1d>Qquerys[0]][0]
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(q_radius_1d, PS_toplot_1d,c='k',marker="*")
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        # Interpolating PS values
+        for i in range(1,len(Qquerys)):
+            PSright=PS_toplot_1d[q_radius_1d>Qquerys[i]][0]
+            PSleft=PS_toplot_1d[q_radius_1d<Qquerys[i]][-1]
+            Qright=q_radius_1d[q_radius_1d>Qquerys[i]][0]
+            Qleft=q_radius_1d[q_radius_1d<Qquerys[i]][-1]
+            PSquerys[i]=PSleft+(PSright-PSleft)*(Qquerys[i]-Qleft)/(Qright-Qleft)
+        ## Plotting the used PS values 
+        ax.scatter(Qquerys, PSquerys,c='r',marker="o")
+        PSquerys=np.log10(PSquerys)
+
+        surface_kt=surface/np.max(surface)
+        n0,bin0 = np.histogram(surface_kt.reshape((1,-1)), density=True,bins=30)
+        bin0=(bin0[1:]+bin0[:-1])/2
+
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(1, 1, 1)
+        ax2.plot(bin99/np.max(bin99), n99*self.kt,c='k')
+        ax2.scatter(bin0,n0,marker="o",c="r")
+
+        return np.concatenate((np.array([self.kt/k99,lambda_0,lambda_1]),n0,PSquerys),axis=None)
+
+def predict(surface_input,n_models=50):
+    # Select root folder of the model members
+    file_path = filedialog.askdirectory()
+    predict_train=[]
+    ## predict one-by-one
+    for i in range(n_models):
+        Model=load_model(file_path+"/model"+str(i))
+        predict_train.append(Model.predict(surface_input))
+    predict_train=np.array(predict_train)
+    predict_uncertainty=np.std(predict_train)
+    prediction=np.mean(predict_train)
+    return prediction,predict_uncertainty
--- README.md
+++ README.md
@@ -1,2 +1,185 @@
+<a name="readme-top"></a>
+
 # Active-learning-codes
-This repository is linked to the research article "Prediction of equivalent sand-grain size and identification of drag-relevant scales of roughness - a data driven approach" (2023) by J. Yang, A. Stroh, S. Lee, S. Bagheri, B. Frohnapfel and P. Forooghi.
+This repository is linked to the research article J. Yang, A. Stroh, S. Lee, S. Bagheri, B. Frohnapfel and P. Forooghi. (2023) Prediction of equivalent sand-grain size and identification of drag-relevant scales of roughness - a data driven approach <em>JFM</em>.
+[![PS_PDF_NeuralNetwork](/0_assets/NN.png)](https://arxiv.org/abs/2304.08958)
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+            <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#Folders">Folders</a></li>
+    <li><a href="#citation">Citation</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#license">License</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+The present project contains python scripts range from [reading roughness statistics](#rgh_category)
+,  [Bayesian optimization of NN architecture](#Bayesian_optimization) and [Bayesian optimization of NN architecture](#enn_training).
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+### Built With
+
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" width="30" height="30" title="Python"/>  &nbsp; &nbsp; v 3.8
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+The present repository contains jupyter notbooks scripts.
+
+### Prerequisites
+
+Python 3.8 along with following Python packages are required for running the code
+
+* Numpy
+  ```bash
+    pip3 install numpy==1.21.5
+  ```
+* Scipy
+  ```bash
+    pip3 install scipy==1.7.3
+  ```
+
+* Pandas
+  ```bash
+    pip3 install pandas==1.4.1
+  ```
+
+* Matplotlib
+  ```bash
+    pip3 install matplotlib==3.6.2
+  ```
+
+* TensorFlow
+  ```bash
+    pip3 install tensorflow==2.10.0
+  ```
+
+* scikit-learn
+  ```bash
+    pip3 install -U scikit-learn==1.0.2
+  ```
+* scikit-optimize
+  ```bash
+    pip3 install scikit-optimize==0.9.0
+  ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Installation
+
+Clone the repository
+   ```sh
+   git clone https://github.com/JiashengY/Active-learning-codes.git
+  ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+## Usage
+### rgh Python class
+**rgh** is a Python class that provides a convenient and efficient way to perform a variety of operations related to roughness analysis.
+You can move the rgh_class.py file from this repository and place it in your project directory. Import **rgh** class:
+```python 
+import rgh_class as rgh
+```
+Create an instance of the rgh class:
+```python 
+surface=rgh.rgh(x,z,y) # x,z are streamwise, spanwise coordinate, y is 2-D roughness map
+```
+
+### Methods
+The **rgh** class provides the following methods:
+* **print_stat()** : Generate a dictionary containing roughness statistical parameters
+* **show_surface(representation="2D")** : Plot surface geometry.
+  * **representation** : String, shoing the representation of roughness geometry either in "2D" or "3D".
+* **plot_PDF(n_bins=10,Normalization=True)** : Present roughness height PDF, where:
+  * **n_bins**: integer, number of bins. 
+  * **Normalization** : boolean, if the plot is noramalized to density.
+* **plot_PS(Normalization=True,circular_average=False,moving_average=False)**: Present roughness height PS, where:
+  * **Normalization** : boolean, if the PS is noramalized with root-mean-square roughness height. 
+  * **azimuthal_average=False**: azimuthally averaged PS around origin of spectral space i.e. (qx,qz)=(0,0), to acquire 1-d PS. 
+  * **moving_average=False**: Once ayimuthal average is done, moving average over 1-d PS can be carried out on demand. 
+  * **n_iter**: number of moving averaging iterations.
+* **FFT_filter(lmax,lmin)**: Spectral filtering of the surface, **lmax**, **lmin** are the desired largest and smallest roughness wavelengths, respectively.
+* **get_model_input(lmax=2,lmin=0.04,azimuthal_average=False,moving_average=False,n_iter=3)**: Generate input vector for the ML model, where:
+  * **lmax**: largest incorporated wavelength
+  * **lmin**: smallest incorporated wavelength
+  * **azimuthal_average=False**: azimuthally averaged PS around origin of spectral space i.e. (qx,qz)=(0,0), to acquire 1-d PS. 
+  * **moving_average=False**: Once ayimuthal average is done, moving average over 1-d PS can be carried out on demand. 
+  * **n_iter**: number of moving averaging iterations.
+
+### Roughness prediction
+**predict(surface_input,n_models=50)** is the function to predict given inputs with the ensemble neural network.
+* **surface_input** is the input vector for the current model, this can be obtained by **get_model_input** method in **rgh** class. For instance:
+
+```python
+prediction=rgh.predict(surface.get_model_input())
+```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- Folders -->
+## Folders
+
+* #### rgh_class 
+This folder contains roughness class **rgh**, **rgh** is a Python class that provides a convenient and efficient way to perform a variety of operations related to roughness analysis.
+* #### Bayesian_optimization
+This folder contains BO routine for optimizing NN archeticture for the current task setup. Output file <strong>Hyperparameters_BO.csv</strong>
+* #### enn_training
+This folder contains ENN training script based on  user specified NN architecture documented in <strong>Hyperparameters.csv</strong>, either generated from BO process or user-defined.
+
+
+<!-- ciation-->
+## Citation
+
+```
+@misc{yang2023prediction,
+      title={Prediction of equivalent sand-grain size and identification of drag-relevant scales of roughness -- a data driven approach}, 
+      author={Jiasheng Yang and Alexander Stroh and Sangseung Lee and Shervin Bagheri and Bettina Frohnapfel and Pourya Forooghi},
+      year={2023},
+      eprint={2304.08958},
+      archivePrefix={arXiv},
+      primaryClass={physics.flu-dyn}
+}
+```
+
+<!-- contact -->
+## Contact
+[Pourya Forooghi](https://pure.au.dk/portal/en/persons/pourya-forooghi(22c2cfbf-081f-4494-b545-45ef29ae5d0f).html) @ Aarhus University
+
+[Alexander Stroh](https://www.istm.kit.edu/558_522.php) @ KIT
+
+[Jiasheng Yang](https://www.istm.kit.edu/558_1459.php) @ KIT
+<!-- license -->
+## License
+
+This repository is released under the [MIT license](https://choosealicense.com/licenses/mit/)
+
+
+
