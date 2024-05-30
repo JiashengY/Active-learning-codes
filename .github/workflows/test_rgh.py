d8ed8b83836efa7ab26a17b248743ddb61223c9a
JiashengY <root@DESKTOP-FBBJS3P>
2024-05-30 10:51:27 +0200

    update code

--- .github/workflows/test_rgh.py
+++ .github/workflows/test_rgh.py
@@ -3,13 +3,13 @@ sys.path.insert(1,'./1_rgh_class')
 import rgh_class as rgh
 import numpy as np
 
-x=np.linspace(0,5,1000)
-z=np.linspace(0,5,1000)
+x=np.linspace(0,4,1000)
+z=np.linspace(0,4,1000)
 Gridx,Gridz=np.meshgrid(x,z)
-y=0.5*np.sin(Gridx)+0.5*np.sin(Gridz)
+y=0.5*np.sin(Gridx*np.pi)+0.5*np.sin(Gridz*np.pi)
 try:
   surface=rgh.rgh(x,z,y)
 except:
   assert False , "rgh instance is not properly created"
-DF_surface_statistics=surface.print_stat()
-assert ~DF_surface_statistics.isnull().values.any()
+metrics=[surface.Lx,surface.Lz,surface.sk,surface.ku,surface.krms,surface.kmd,surface.kt,surface.k99,surface.ra,surface.por,surface.ESx,surface.ESz,surface.incx,surface.incz]
+assert ~any(np.isnan(metrics)) , "went wrong when calculating roughness metrics"
