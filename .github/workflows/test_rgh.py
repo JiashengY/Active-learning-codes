2e84918057877e2163d937de2228383050ff4e74
JiashengY <root@DESKTOP-FBBJS3P>
2024-05-30 09:51:39 +0200

    changed CI_yml

---
+++ .github/workflows/test_rgh.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(1,'../../1_rgh_class')
+import rgh_class as rgh
+import numpy as np
+
+x=np.linspace(0,5,1000)
+z=np.linspace(0,5,1000)
+Gridx,Gridz=np.meshgrid(x,z)
+y=0.5*np.sin(Gridx)+0.5*np.sin(Gridz)
+try:
+  surface=rgh.rgh(x,z,y)
+except:
+  assert False , "rgh instance is not properly created"
+DF_surface_statistics=surface.print_stat()
+assert ~DF_surface_statistics.isnull().values.any()
